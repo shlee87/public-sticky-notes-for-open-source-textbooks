4ebff3dc512a3f24d5690d589a48ed2c66e73780
CasHollis <emh031@email.latech.edu>
2023-02-06 12:08:57 -0600

    Can enter and submit notes to webpage

--- sticky-chrome-extension/background.js
+++ sticky-chrome-extension/background.js
@@ -4,12 +4,174 @@ chrome.action.onClicked.addListener(execScript);
 async function execScript() {
   const tabId = await getTabId();
   chrome.scripting.executeScript({
-    target: {tabId: tabId},
+    target: { tabId: tabId },
     files: ['execute.js']
   })
 }
 
 async function getTabId() {
-  const tabs = await chrome.tabs.query({active: true, currentWindow: true});
+  const tabs = await chrome.tabs.query({ active: true, currentWindow: true });
   return (tabs.length > 0) ? tabs[0].id : null;
+}
+
+// console.log(chrome)
+// helpers(chrome);
+chrome.contextMenus.onClicked.addListener((onClickData, tabInfo) => {
+  switch (onClickData.menuItemId) {
+    case 'show_note_ex_form': {
+      chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        console.log("send message show_note_ex_form");
+        chrome.tabs.sendMessage(tabs[0].id, { title: "SHOW_FORM", payload: null });
+      });
+      break;
+    }
+    case "history_context": {
+      console.log("history listener");
+      historyContextClickHandler(onClickData, tabInfo);
+
+      break;
+    }
+    case "clear_context": {
+      clearHistory();
+    }
+  }
+});
+
+chrome.runtime.onMessage.addListener((req, sender, res) => {
+  //update context menu with selection payload
+  if (req.title == "TEXT_SELECTED") {
+    console.log("responding to text_selected msg");
+    chrome.contextMenus.update("show_note_ex_form", { title: `Translate: ${req.payload}`, onclick: translateContextClickHandler });
+  }
+});
+
+
+chrome.runtime.onInstalled.addListener(async function (tab, statusInfo, response) {
+  //create our context menus
+  _contexts = [
+    { id: "show_note_ex_form", title: "Create Notes", contexts: ["all"] },
+    { id: "history_context", title: "See notes", contexts: ["all"] },
+    { id: "clear_context", title: "Clear history", contexts: ["all"] },
+  ];
+
+  _contexts.forEach((ctx) => {
+    chrome.contextMenus.create(ctx, () => {
+      console.log("ctx created", ctx);
+    });
+  });
+
+  // updateHistoryContext();
+});
+
+function helpers(chrome) {
+
+  const listen = (chrome) => {
+
+    chrome.runtime.onMessage.addListener((req, sender, res) => {
+      //update context menu with selection payload
+      if (req.title == "TEXT_SELECTED") {
+        console.log("responding to text_selected msg");
+        chrome.contextMenus.update("show_note_ex_form", { title: `Translate: ${req.payload}`, onclick: translateContextClickHandler });
+      }
+    });
+  }
+
+  const setupContextEventListener = (chrome) => {
+
+    // chrome.contextMenus.onClicked.addListener((onClickData, tabInfo) => {
+    //   switch (onClickData.menuItemId) {
+    //     case "history_context": {
+    //       console.log("history listener");
+    //       historyContextClickHandler(onClickData, tabInfo);
+
+    //       break;
+    //     }
+    //     case "clear_context": {
+    //       clearHistory();
+    //     }
+    //   }
+    // });
+  }
+
+
+
+  const populateContextMenusAndHandleEvents = (chrome) => {
+
+    chrome.runtime.onInstalled.addListener(async function (tab, statusInfo, response) {
+      //create our context menus
+      _contexts = [
+        { id: "show_note_ex_form", title: "Create Notes", contexts: ["all"] },
+        { id: "history_context", title: "See notes", contexts: ["all"] },
+        { id: "clear_context", title: "Clear history", contexts: ["all"] },
+      ];
+
+      _contexts.forEach((ctx) => {
+        chrome.contextMenus.create(ctx, () => {
+          console.log("ctx created", ctx);
+        });
+      });
+
+      // updateHistoryContext();
+    });
+
+    setupContextEventListener();
+  }
+
+
+
+
+
+
+  const init = (chrome) => {
+    listen(chrome);
+    populateContextMenusAndHandleEvents(chrome);
+  }
+
+  init();
+
+
+  function translateContextClickHandler(onClickData, tabInfo) {
+    translateWord(onClickData.selectionText ?? "");
+  }
+
+
+  async function historyContextClickHandler(onClickData, tabInfo) {
+    chrome.storage.sync.get(["historic_tranlations"], (storage) => {
+      chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        console.log("send message historic_tranlations", storage.historic_tranlations);
+        chrome.tabs.sendMessage(tabs[0].id, { title: "SHOW_HISTORY", payload: storage.historic_tranlations });
+      });
+    });
+  }
+
+
+  function saveTranslation(query, result) {
+    chrome.storage.sync.get(["historic_tranlations"], ({ historic_tranlations = {} }) => {
+      // storage.historic_tranlations = storage.historic_tranlations ?? {};
+      historic_tranlations[query] = { input: query, output: result };
+      chrome.storage.sync.set({ historic_tranlations }, () => {
+        console.log("saved");
+        // updateHistoryContext();
+      });
+    });
+  }
+
+  async function getHistoryTranslations() {
+    let { historic_tranlations } = await chrome.storage.sync.get(["historic_tranlations"]);
+    return historic_tranlations;
+  }
+
+  function clearHistory() {
+    console.log("clearing history");
+    chrome.storage.sync.set({ historic_tranlations: {} }, () => {
+      // updateHistoryContext();
+    });
+  }
+
+  function updateHistoryContext() {
+    chrome.storage.sync.get(["historic_tranlations"], ({ historic_tranlations }) => {
+      chrome.contextMenus.update("history_context", { title: `Translation History ${Object.values(historic_tranlations).length}`, onclick: historyContextClickHandler });
+    });
+  }
+
 }
---
+++ sticky-chrome-extension/contentScript-main.js
@@ -0,0 +1,132 @@
+// this code will be executed before page load
+(function () {
+    console.log('before.js executed');
+    insertHtml();
+})();
+
+function send(title, payload) {
+
+    chrome.runtime.sendMessage({ title, payload });
+}
+
+function listen() {
+    chrome.runtime.onMessage.addListener((req, sender, res) => {
+        console.log("responding to render message");
+
+        switch (req.title) {
+            case "SHOW_TRANSLATION": {
+                showTranslationModal(req);
+                break;
+            }
+
+            case "SHOW_FORM": {
+                showForm(req);
+                break;
+            }
+
+            case 'HIDE_FORM': {
+                hideForm();
+                break;
+            }
+            default: {
+                console.log("No a valid SpringCompanion Option");
+            }
+        }
+    });
+}
+
+function showForm() {
+    hideForm();
+    document.querySelector('[note-ex-form]').classList.add('show')
+}
+
+function hideForm() {
+    document.querySelector('[note-ex-form]').classList.remove('show')
+}
+
+function insertHtml() {
+
+    const style = `
+[note-ex-form] {
+        position: fixed;
+        z-index: 1000;
+        top: 0;
+        margin: 20px;
+        background-color: #2c5b2c;
+        visibility: hidden;
+        padding: 10px;
+    }
+
+    [note-ex-form].show {
+        visibility: initial;
+    }
+
+    [note-ex-form] :is(input,textarea){
+        padding: 3px;
+    }
+   `
+
+    const content = `
+    <form note-ex-form class="flex-col" style="display:flex;flex-direction: column;gap: 10px;" onsubmit="noteExInsert()">
+    <span>
+        Write note here
+    </span>
+    <input type="text" id="title" placeholder="title"></input>
+    <textarea name="note" id="" cols="30" rows="10" onkeyup="Allow()" placeholder="note..."></textarea>
+    <!-- <input id="note" type="text" maxlength="500" onkeyup="Allow()"> -->
+    <input type="submit" value="Save/Show" />
+   
+
+</form>`;
+
+    const script = `
+    <script type="text/javascript">
+    var notes = [];
+    var noteInput = document.getElementById("note");
+    var messageBox = document.getElementById("display");
+
+    function Allow() {
+        if (!user.note.value.match(/[a-zA-Z]$/) && user.note.value != "") {
+            user.note.value = "";
+            alert("Please Enter only alphabets");
+        }
+    }
+
+    function noteExInsert() {
+        notes.push(noteInput.value);
+        NoteExclearAndShow();
+    }
+
+    function NoteExclearAndShow() {
+        noteInput.value = "";
+        messageBox.innerHTML = "";
+        messageBox.innerHTML += "Notes: <br/><br/>" + notes.join("<br/><br/>");
+        document.querySelector(['note-ex-form']).classlist.remove('show')
+    }
+</script>`;
+
+    const makeElement = (tag, innerHtml,/** @type {{}}*/attributes) => {
+        /** @type {HTMLElement} */
+        const element = document.createElement(tag);
+        element.innerHTML = innerHtml;
+
+        if (attributes && Object.values(attributes).length) {
+            //add attributes to the element
+            Object.entries(attributes).forEach(([attName, attValue]) => {
+                element.setAttribute(attName, attValue)
+            })
+        }
+
+        return element;
+    }
+
+    const body = document.querySelector('body')
+
+    const formElement = makeElement('div', content, { 'notexe-new-form': '' });
+    body.append(formElement)
+    body.append(makeElement('style', style))
+    console.log('notex-insertion complete')
+
+}
+
+listen()
