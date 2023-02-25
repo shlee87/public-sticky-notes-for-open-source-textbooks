bfdb20bc565992455c3eef05ea0c40c430dd9ca7
DDYoung004 <114763412+DDYoung004@users.noreply.github.com>
2023-02-25 11:59:37 -0600

    Less errors?

---
+++ sticky-chrome-extension/script.js
@@ -0,0 +1,28 @@
+import Amplify, { Auth } from './aws-amplify';
+
+// Configure Amplify
+Amplify.configure({
+    Auth: {
+        region: 'us-west-2',
+        userPoolId: 'us-west-2_ABC123',
+        userPoolWebClientId: '12345abcdef',
+    }
+});
+
+// Render login component
+const loginComponent = AmplifyUI.signIn({});
+document.getElementById('login').appendChild(loginComponent);
+
+// Handle login submission
+const loginForm = document.getElementById('signinForm');
+loginForm.addEventListener('submit', async (event) => {
+    event.preventDefault();
+    const username = loginForm.querySelector('input[name="username"]').value;
+    const password = loginForm.querySelector('input[name="password"]').value;
+    try {
+        await Auth.signIn(username, password);
+        console.log('Logged in successfully');
+    } catch (error) {
+        console.error('Login error:', error);
+    }
+});
