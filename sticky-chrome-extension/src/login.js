98e7dbbf6bf01cd3bfa3e8926cb055df3b6257d1
DDYoung004 <114763412+DDYoung004@users.noreply.github.com>
2023-02-26 14:37:58 -0600

    halleuja

--- sticky-chrome-extension/src/login.js
+++ sticky-chrome-extension/src/login.js
@@ -3,11 +3,44 @@ import { Amplify, Auth } from 'aws-amplify';
 // Configure Amplify with your Auth settings
 Amplify.configure({
     Auth: {
+
+        // REQUIRED only for Federated Authentication - Amazon Cognito Identity Pool ID
+        identityPoolId: 'us-east-2:71482aac-b3f0-462e-b87e-3050b8fa6743',
+
+        // REQUIRED - Amazon Cognito Region
         region: 'us-east-2',
+
+        // OPTIONAL - Amazon Cognito User Pool ID
         userPoolId: 'us-east-2_msJcUyPZi',
+
+        // OPTIONAL - Amazon Cognito Web Client ID (26-char alphanumeric string)
         userPoolWebClientId: '5mm44lds4fsqcao4kdnp0k68ch',
-    },
+
+        // OPTIONAL - Enforce user authentication prior to accessing AWS resources or not
+        mandatorySignIn: true,
+    }
 });
+
+Auth.configure({
+    Auth: {
+
+        // REQUIRED only for Federated Authentication - Amazon Cognito Identity Pool ID
+        identityPoolId: 'us-east-2:71482aac-b3f0-462e-b87e-3050b8fa6743',
+
+        // REQUIRED - Amazon Cognito Region
+        region: 'us-east-2',
+
+        // OPTIONAL - Amazon Cognito User Pool ID
+        userPoolId: 'us-east-2_msJcUyPZi',
+
+        // OPTIONAL - Amazon Cognito Web Client ID (26-char alphanumeric string)
+        userPoolWebClientId: '6ej3v8r198u7q1nr5l6usg1fqa',
+
+        // OPTIONAL - Enforce user authentication prior to accessing AWS resources or not
+        mandatorySignIn: true,
+    }
+});
+
 
 // Define the sign-in configuration options
 const signInConfig = {
