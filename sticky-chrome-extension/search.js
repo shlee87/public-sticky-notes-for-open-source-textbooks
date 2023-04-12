adeaf7e9a5335e222a0a6f32db95cb81c79e2b7b
nks4860 <nks4860@gmail.com>
2023-04-12 09:25:46 -0500

    dd

---
+++ sticky-chrome-extension/search.js
@@ -0,0 +1,33 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const searchForm = document.querySelector(".u-search");
+    const searchInput = document.querySelector(".u-search-input");
+  
+    searchForm.addEventListener("submit", async (event) => {
+      event.preventDefault();
+      const searchTerm = searchInput.value.trim();
+      if (searchTerm.length === 0) {
+        return;
+      }
+  
+      try {
+        const user = await Auth.currentAuthenticatedUser();
+        const token = user.signInUserSession.idToken.jwtToken;
+  
+        const lambdaUrl = "https://uwumu5g5672s7kfcmspjpwukki0nnxez.lambda-url.us-east-1.on.aws/";
+        const response = await fetch(lambdaUrl, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: token,
+          },
+          body: JSON.stringify({ searchTerm }),
+        });
+  
+        const data = await response.json();
+        console.log("Lambda response:", data);
+      } catch (error) {
+        console.error("Error calling Lambda function:", error);
+      }
+    });
+  });
+  
