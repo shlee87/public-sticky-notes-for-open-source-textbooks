34421a43815e38789efc6940dc62f228374e40d2
DDYoung004 <114763412+DDYoung004@users.noreply.github.com>
2023-04-17 16:46:25 -0500

    Setup Tweaks

--- sticky-chrome-extension/src/note.js
+++ sticky-chrome-extension/src/note.js
@@ -1,112 +1,29 @@
+import { Auth } from 'aws-amplify';
+
 const urlParams = new URLSearchParams(window.location.search);
 const pageUrl = urlParams.get('url');
 const paragraph = urlParams.get('par');
-const range = urlParams.get('range');
 const startOffset = urlParams.get('start');
 const endOffset = urlParams.get('end');
 const color = urlParams.get('color');
 const xpath = urlParams.get('xpath');
 
-console.log(pageUrl);
-console.log(paragraph);
-console.log(startOffset);
-console.log(endOffset);
-console.log(color);
 console.log(xpath);
-
 
 const WebText = document.getElementById("WebText");
 WebText.innerHTML = paragraph;
 
-const highlightColor = "rgb(213, 234, 255)";
-
-const highlightTemplate = `
-  <template id="highlightTemplate">
-    <span class="highlight" style="background-color: ${highlightColor}; display: inline"></span>
-  </template>
-
-  <button id="highlightingButton">
-    <svg class="text-marker" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 544 512"><path d="M0 479.98L99.92 512l35.45-35.45-67.04-67.04L0 479.98zm124.61-240.01a36.592 36.592 0 0 0-10.79 38.1l13.05 42.83-50.93 50.94 96.23 96.23 50.86-50.86 42.74 13.08c13.73 4.2 28.65-.01 38.15-10.78l35.55-41.64-173.34-173.34-41.52 35.44zm403.31-160.7l-63.2-63.2c-20.49-20.49-53.38-21.52-75.12-2.35L190.55 183.68l169.77 169.78L530.27 154.4c19.18-21.74 18.15-54.63-2.35-75.13z"></path></svg>
-  </button>
-`;
-
-const highlightStyled = ({ display = "none", left = 0, top = 0 }) => `
-  #highlightingButton {
-    align-items: center;
-    background-color: black;
-    border-radius: 5px;
-    border: none;
-    cursor: pointer;
-    display: ${display};
-    justify-content: center;
-    left: ${left}px;
-    padding: 5px 10px;
-    position: fixed;
-    top: ${top}px;
-    width: 40px;
-    z-index: 9999;
-  }
-  .text-marker {
-    fill: white;
-  }
-  .text-marker:hover {
-    fill: ${highlightColor};
-  }
-`;
-
-class HighlighterClass extends HTMLElement {
-    constructor() {
-        super();
-        this.render();
-    }
-
-    get markerPosition() {
-        return JSON.parse(this.getAttribute("markerPosition") || "{}");
-    }
-
-    get styleElement() {
-        return this.shadowRoot.querySelector("style");
-    }
-
-    get highlightTemplate() {
-        return this.shadowRoot.getElementById("highlightTemplate");
-    }
-
-    static get observedAttributes() {
-        return ["markerPosition"];
-    }
-
-    render() {
-        this.attachShadow({ mode: "open" });
-        const style = document.createElement("style");
-        style.textContent = highlightStyled({});
-        this.shadowRoot.appendChild(style);
-        this.shadowRoot.innerHTML += highlightTemplate;
-        this.shadowRoot
-            .getElementById("highlightingButton")
-            .addEventListener("click", () => this.highlightSelection());
-    }
-
-    attributeChangedCallback(name, oldValue, newValue) {
-        if (name === "markerPosition") {
-            this.styleElement.textContent = highlightStyled(this.markerPosition);
-        }
-    }
-
-    highlightSelection() {
-        var userSelection = window.getSelection();
-        for (let i = 0; i < userSelection.rangeCount; i++) {
-            this.highlightRange(userSelection.getRangeAt(i));
-        }
-        window.getSelection().empty();
-    }
-
-    highlightRange(range) {
-        const clone =
-            this.highlightTemplate.cloneNode(true).content.firstElementChild;
-        clone.appendChild(range.extractContents());
-        range.insertNode(clone);
-    }
-}
-
-window.customElements.define("stickit-highlighter", HighlighterClass);
+Auth.currentAuthenticatedUser()
+    .then((user) => {
+        const userId = user.attributes.sub;
+        // Method for calling the lambda fuction through the url
+        const lambdaUrl = 'https://5a6dwifl33kakbasgv6izm3sda0gpygw.lambda-url.us-east-1.on.aws/';
+        // Method for passing over variables to the lambda function
+        const url = `${ lambdaUrl }/?userId=${encodeURIComponent(userId)}`;
+        // call the function and get the response through the data output
+        fetch(url)
+            .then(response => response.json())
+            .then(data => console.log(data[0].username));
+    })
+    // Catch any errors
+    .catch((error) => console.log(error)); 
