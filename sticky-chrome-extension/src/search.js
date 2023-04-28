22c0b1080b89db9b5132f2547536402aa01896a2
Alephnull-0 <ecg015@email.latech.edu>
2023-04-28 15:42:12 -0500

    Search.js tweaks

--- sticky-chrome-extension/src/search.js
+++ sticky-chrome-extension/src/search.js
@@ -1,3 +1,31 @@
+import { Auth } from 'aws-amplify';
+
+Auth.configure({
+  Auth: {
+
+      // REQUIRED only for Federated Authentication - Amazon Cognito Identity Pool ID
+      identityPoolId: 'us-east-2:71482aac-b3f0-462e-b87e-3050b8fa6743',
+
+      // REQUIRED - Amazon Cognito Region
+      region: 'us-east-2',
+
+      // OPTIONAL - Amazon Cognito User Pool ID
+      userPoolId: 'us-east-2_msJcUyPZi',
+
+      // OPTIONAL - Amazon Cognito Web Client ID (26-char alphanumeric string)
+      userPoolWebClientId: '6ej3v8r198u7q1nr5l6usg1fqa',
+
+      // OPTIONAL - Enforce user authentication prior to accessing AWS resources or not
+      mandatorySignIn: true,
+  }
+});
+
+
+Auth.currentAuthenticatedUser()
+ .then((user) => {
+    const userId = user.attributes.sub;
+ });
+
 document.addEventListener("DOMContentLoaded", function () {
     const searchForm = document.querySelector(".u-search");
     const searchInput = document.querySelector(".u-search-input");
